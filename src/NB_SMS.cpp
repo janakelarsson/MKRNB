6ed279e7006785d9c546e5a6142c24915d1baf14
Jan-Åke Larsson <jan-ake.larsson@liu.se>
2020-06-30 13:49:46 +0200

    Simplify further, document API

--- src/NB_SMS.cpp
+++ src/NB_SMS.cpp
@@ -111,7 +111,7 @@ int NB_SMS::setCharset(const char* charset)
       return 0;
   }
   MODEM.sendf("AT+CSCS?");
-  if (MODEM.waitForResponse(100,readcharset) != 1) {
+  if (MODEM.waitForResponse(100,&readcharset) != 1) {
     return 0;
   _charset=readcharset[0];
   return _charset;
